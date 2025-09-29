141735004fdb5e8f3e02b2049ac7c628a9d9e18f
TI3TC <ti@3tc.com.br>
2025-09-29 14:54:04 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { router } from "./routes";
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+// Rotas
+app.use("/", router);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`🚀 Integration server running at http://localhost:${PORT}`);
+});
 
