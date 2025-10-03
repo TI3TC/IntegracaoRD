79f1f73ed3221a4373886d55ab175d924d79d93d
TI3TC <ti@3tc.com.br>
2025-10-03 15:24:20 -0300

    Update rdstation.service.ts

--- src/services/rdstation.service.ts
+++ src/services/rdstation.service.ts
@@ -7,7 +7,10 @@ export async function handleLeadFromRD(body: any) {
     name: payload?.name || "Sem Nome",
     email: payload?.email,
     phone: payload?.personal_phone || payload?.mobile_phone,
-    event: body?.event_type || payload?.conversion_identifier || "conversion"
+    event: body?.event_type || payload?.conversion_identifier || "conversion",
+    projeto: payload?.cf_conte_mais_sobre_seu_projeto,
+    aplicacao: payload?.cf_aplicacao,
+    fj:payload?.cf_pessoa_fisica_juridica
   };
 
   if (!lead.email) {
